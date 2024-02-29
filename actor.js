b236a5de4162e49ca56fe94b6881306c889264f5
agamis28 <agamis@rrc.ca>
2024-02-29 15:17:53 -0600

    Creating actor class that takes in basic starting actor variables and size

---
+++ actor.js
@@ -0,0 +1,8 @@
+class Actor {
+  constructor(startingPosition, startingVelocity, startingRotation, size) {
+    this.position = startingPosition;
+    this.velocity = startingVelocity;
+    this.rotation = startingRotation;
+    this.size = size;
+  }
+}
