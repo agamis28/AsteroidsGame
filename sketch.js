92503fb3f1413be9cf4a3bbeff5c7c215ed6ef99
agamis28 <agamis@rrc.ca>
2024-03-04 16:34:46 -0600

    Commenting code in sketch script

--- sketch.js
+++ sketch.js
@@ -5,11 +5,10 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+// }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background(220);
-}
+// function draw() {
+//   background(220);
+// }
